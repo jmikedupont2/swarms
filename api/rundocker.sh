0dea58925604fe65e2ff01fb0702d7a8df1e0e82
mike dupont <mike.dupont@introspector.local>
2024-12-25 14:33:19 -0500

    update

--- api/rundocker.sh
+++ api/rundocker.sh
@@ -7,60 +7,69 @@
 # this is the refresh script called from ssm for a refresh
 #  #refresh_script = "/opt/swarms/api/docker-boot.sh" 
 
+# file not found
+#
+pwd
+ls -latr
 . ./.env # for secrets
 set -e # stop  on any error
-export ROOT="" # empty
-export WORKSOURCE="${ROOT}/opt/swarms/api"
+#export ROOT="" # empty
+export WORKSOURCE="/opt/swarms/api"
 
-adduser --disabled-password --gecos "" swarms --home "${ROOT}/home/swarms"  || echo ignore
-git config --global --add safe.directory "${ROOT}/opt/swarms"
-git config --global --add safe.directory "${ROOT}/opt/swarms-memory"
+adduser --disabled-password --gecos "" swarms --home "/home/swarms"  || echo ignore
+git config --global --add safe.directory "/opt/swarms"
+git config --global --add safe.directory "/opt/swarms-memory"
 
-cd "${ROOT}/opt/swarms/" || exit 1 # "we need swarms"
+cd "/opt/swarms/" || exit 1 # "we need swarms"
 git log -2 --patch | head  -1000
 
-mkdir -p "${ROOT}/var/swarms/agent_workspace/"
-mkdir -p "${ROOT}/home/swarms"
+mkdir -p "/var/swarms/agent_workspace/"
+mkdir -p "/home/swarms"
 
 
-cd "${ROOT}/opt/swarms/" || exit 1 # "we need swarms"
+cd "/opt/swarms/" || exit 1 # "we need swarms"
 
-mkdir -p "${ROOT}/var/swarms/logs"
-chmod +x "${ROOT}/var/swarms/agent_workspace/boot_fast.sh"
-chown -R swarms:swarms "${ROOT}/var/swarms/" "${ROOT}/home/swarms" "${ROOT}/opt/swarms"
+mkdir -p "/var/swarms/logs"
+chown -R swarms:swarms "/var/swarms/" "/home/swarms" "/opt/swarms"
 
-# user install but do not start
-su -c "bash -e -x ${ROOT}/var/swarms/agent_workspace/boot_fast.sh" swarms
+if [ -f "/var/swarms/agent_workspace/boot_fast.sh" ];
+then
+    chmod +x "/var/swarms/agent_workspace/boot_fast.sh" || echo faild
+    
+    # user install but do not start
+    su -c "bash -e -x /var/swarms/agent_workspace/boot_fast.sh" swarms
+fi
+cd "/opt/swarms/" || exit 1 # "we need swarms"
 
-cd "${ROOT}/opt/swarms/" || exit 1 # "we need swarms"
-
-mkdir -p "${ROOT}/var/run/swarms/secrets/"
-mkdir -p "${ROOT}/home/swarms/.cache/huggingface/hub"
-export OPENAI_KEY=`aws ssm get-parameter     --name "swarms_openai_key" | jq .Parameter.Value -r `
-echo "OPENAI_KEY=${OPENAI_KEY}" > "${ROOT}/var/run/swarms/secrets/env"
+mkdir -p "/var/run/swarms/secrets/"
+mkdir -p "/home/swarms/.cache/huggingface/hub"
+OPENAI_KEY=$(aws ssm get-parameter     --name "swarms_openai_key" | jq .Parameter.Value -r )
+export OPENAI_KEY
+echo "OPENAI_KEY=${OPENAI_KEY}" > "/var/run/swarms/secrets/env"
 
 ## append new homedir
 check if the entry exists already before appending pls
-if ! grep -q "HF_HOME" ${ROOT}/var/run/swarms/secrets/env"; then
-       echo "HF_HOME=${ROOT}/home/swarms/.cache/huggingface/hub" >> "${ROOT}/var/run/swarms/secrets/env"
+if ! grep -q "HF_HOME" "/var/run/swarms/secrets/env"; then
+       echo "HF_HOME=/home/swarms/.cache/huggingface/hub" >> "/var/run/swarms/secrets/env"
 fi
 
-if ! grep -q "^HOME" ${ROOT}/var/run/swarms/secrets/env"; then
-    echo "HOME=${ROOT}/home/swarms" >> "${ROOT}/var/run/swarms/secrets/env"
+if ! grep -q "^HOME" "/var/run/swarms/secrets/env"; then
+    echo "HOME=/home/swarms" >> "/var/run/swarms/secrets/env"
 fi
 
-if ! grep -q "^HOME" ${ROOT}/var/run/swarms/secrets/env"; then
+if ! grep -q "^HOME" "/var/run/swarms/secrets/env"; then
 # attempt to move the workspace
-echo 'WORKSPACE_DIR=${STATE_DIRECTORY}' >> "${ROOT}/var/run/swarms/secrets/env"
+    echo "WORKSPACE_DIR=\${STATE_DIRECTORY}" >> "/var/run/swarms/secrets/env"
+fi
 
 # setup the systemd service again
-sed -e "s!ROOT!${ROOT}!g" > /etc/nginx/sites-enabled/default < "${WORKSOURCE}/nginx/site.conf"
-sed -e "s!ROOT!${ROOT}!g" > /etc/systemd/system/swarms-docker.service < "${WORKSOURCE}/systemd/swarms-docker.service"
+sed -e "s!ROOT!!g" > /etc/nginx/sites-enabled/default < "${WORKSOURCE}/nginx/site.conf"
+sed -e "s!ROOT!!g" > /etc/systemd/system/swarms-docker.service < "${WORKSOURCE}/systemd/swarms-docker.service"
 grep . -h -n /etc/systemd/system/swarms-docker.service
 
-chown -R swarms:swarms ${ROOT}/var/run/swarms/
-mkdir -p ${ROOT}/opt/swarms/api/agent_workspace/try_except_wrapper/
-chown -R swarms:swarms ${ROOT}/opt/swarms/api/
+chown -R swarms:swarms /var/run/swarms/
+mkdir -p /opt/swarms/api/agent_workspace/try_except_wrapper/
+chown -R swarms:swarms /opt/swarms/api/
 
 # always reload
 systemctl daemon-reload
@@ -79,6 +88,6 @@ while ! nc -z $HOST $PORT; do
   sleep 1
   echo -n "."
 done
-echo "Port $PORT is now open!"
+echo "Port ${PORT} is now open!"
 
-systemctl restart nginx
+osystemctl restart nginx
